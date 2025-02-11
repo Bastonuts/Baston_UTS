46599f5ab54e3659282fab481d27aceb1abaf41a
gnoriega82 <gnoriega@uts.edu.co>
2025-02-10 22:32:47 -0500

    Nueva Db

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ export const PORT = process.env.PORT || 3000
 
 export const DB_HOST = process.env.DB_HOST || 'autorack.proxy.rlwy.net'
 export const DB_USER = process.env.DB_USER || 'root'
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'RYiLokKTyzJUcLfJrNAxAdZkooFLglVf'
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'AVGPLtEzoYgvtuRwCJLycYBkJLOolnQl'
 export const DB_NAME = process.env.DB_NAME || 'railway'
-export const DB_PORT = process.env.DB_PORT || 17724
+export const DB_PORT = process.env.DB_PORT || 57361
