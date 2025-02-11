6caff5f5cef895690afce50f83625a4993945da8
gnoriega82 <gnoriega@uts.edu.co>
2025-02-10 21:24:58 -0500

    Variables DB

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const PORT = process.env.PORT || 3000
 
-export const DB_HOST = process.env.DB_HOST || 'junction.proxy.rlwy.net'
+export const DB_HOST = process.env.DB_HOST || 'autorack.proxy.rlwy.net'
 export const DB_USER = process.env.DB_USER || 'root'
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'dmzLWlPlOIiMGMjokiKpkrQIWrokPQsq'
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'RYiLokKTyzJUcLfJrNAxAdZkooFLglVf'
 export const DB_NAME = process.env.DB_NAME || 'railway'
-export const DB_PORT = process.env.DB_PORT || 50143
+export const DB_PORT = process.env.DB_PORT || 17724
